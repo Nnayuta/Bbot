00699e6b85aecaa14ee691e70ef0519538107463
Nayuta <naayudan@gmail.com>
2022-01-06 09:35:21 -0300

    fix texts

--- index.py
+++ index.py
@@ -6,6 +6,13 @@ import random
 workOn = False
 mapCount = 0
 
+timeString = " [%H:%M:%S]"
+def botSay(text):
+    """
+    Função de log para o bot personalizada.
+    """
+    print(time.strftime(timeString) + ' - [Bombot] > ' + text)
+
 def click(x,y):
     """
     Função para clicar em um local específico.
@@ -31,20 +38,24 @@ def checkServerStatus():
     """
     if pyautogui.locateOnScreen('./src/statusMaintenace.png', confidence=0.9) != None or pyautogui.locateOnScreen('./src/statusOffline.png', confidence=0.9) != None:
         while True:
-            print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Verificando o status do servidor...')
+            botSay('Verificando o status do servidor...')
             if pyautogui.locateOnScreen('./src/statusOnline.png') != None:
-                print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Servidor voltou ao normal.')
+                botSay('Servidor online!')
                 return True
             else:
                 if pyautogui.locateOnScreen('./src/statusMaintenace.png', confidence=0.9) != None:
-                    print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Servidor em manutenção.')
+                    botSay('Servidor em manutenção!')
                 elif pyautogui.locateOnScreen('./src/statusOnline.png', confidence=0.9) != None:
-                    print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Servidor offline.')
+                    botSay('Servidor offline!')
                 time.sleep(returnSeconds(2)) #Verifica a cada 2 minutos.
 
     elif pyautogui.locateOnScreen('./src/statusOnline.png', confidence=0.9) != None:
-        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Servidor online.')
+        botSay('Servidor online!')
         return True
+    else:
+        botSay('Não consigo ver o jogo. Ele precisa esta em primeiro plano.')
+        time.sleep(10)
+
 
 while checkServerStatus():
 
@@ -66,20 +77,20 @@ while checkServerStatus():
     if pyautogui.locateOnScreen('./src/ConnectButton.png', grayscale=True, confidence=0.8 ) != None:
         herobutton = pyautogui.locateOnScreen('./src/ConnectButton.png', grayscale=True, confidence=0.8 )
         x,y = pyautogui.center(herobutton)
-        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Fazendo login no jogo.')
+        botSay('Conectando ao jogo...')
         click(x,y)
         time.sleep(random.uniform(10,15))
         #Verifica se tem o botão da metamesk para se clica em conectar no jogo
         if pyautogui.locateOnScreen('./src/MetamasLogin.png', grayscale=True, confidence=0.8 ) != None:
             herobutton = pyautogui.locateOnScreen('./src/MetamasLogin.png', grayscale=True, confidence=0.8 )
             x,y = pyautogui.center(herobutton)
-            print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Assinando MetaMesk')
+            botSay('Assinando a metamask')
             click(x,y)
             time.sleep(random.uniform(15,20))
         if pyautogui.locateOnScreen('./src/UserNotLogged.png', grayscale=True, confidence=0.8 ) != None:
             okButton = pyautogui.locateOnScreen('./src/ErrOK.png', grayscale=True, confidence=0.8 )
             x,y = pyautogui.center(okButton)
-            print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Falha ao logar no jogo. Tentando novamente.')
+            botSay('Falha ao logar no jogo. Tentando novamente...')
             click(x,y)
             time.sleep(random.uniform(15,20))
             break
@@ -90,7 +101,7 @@ while checkServerStatus():
         if pyautogui.locateOnScreen('./src/herobutton.png', grayscale=True, confidence=0.8) != None:
             heroBtn = pyautogui.locateOnScreen('./src/herobutton.png', grayscale=True, confidence=0.8 )
             x,y = pyautogui.center(heroBtn)
-            print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Indo para o menu de herois.')
+            botSay('Indo para o menu de herois.')
             click(x,y)
             time.sleep(random.uniform(2,3))
 
@@ -98,43 +109,43 @@ while checkServerStatus():
         if pyautogui.locateOnScreen('./src/maxStamina.png') != None:
             workAll = pyautogui.locateOnScreen('./src/workAll.png', grayscale=True, confidence=0.8)
             x,y = pyautogui.center(workAll)
-            print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Heroi(s) com stamina full. Colocando todos os herois para trabalhar.')
+            botSay('Colocando todos os herois para trabalhar.')
             click(x,y)
             workOn = True
             time.sleep(random.uniform(2,3))
         else:
-            print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Não tem heroi com stamina full.')
+            botSay('Não tem herois com stamina full.')
             time.sleep(random.uniform(1,2))
             if pyautogui.locateOnScreen('./src/workOn.png') != None:
                 workOn = True
-                print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Encontrei heroi(s) em Work.')
+                botSay('Encontrei heroi(s) em Work.')
             else:
                 workOn = False
                 time_duration = random.uniform(returnSeconds(4),returnSeconds(5)) #Tempo em minutos que a janela vai fica aguardando um heroi fica full. apos isso feche e abra novamente.
                 time_start = time.time()
 
                 while time.time() - time_start < time_duration:
-                    print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Aguardando heroi ficar com Stamina full. ou algum heroi em Work.')
+                    botSay('Aguardando heroi ficar com Stamina full. ou algum heroi em Work.')
 
                     if pyautogui.locateOnScreen('./src/workOn.png') != None:
-                        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Encontrei um heroi em Work.')
+                        botSay('Encontrei um heroi em Work.')
                         break
                     
                     if pyautogui.locateOnScreen('./src/maxStamina.png') != None:
-                        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Um heroi ou mais acabou de carregar stamina.')
+                        botSay('Um heroi ou mais acabou de carregar stamina.')
                         break
                     time.sleep(30)
             
         xBtn = pyautogui.locateOnScreen('./src/xBtn.png', grayscale=True, confidence=0.8)
         x,y = pyautogui.center(xBtn)
-        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Fechando janela de herois.')
+        botSay('Fechando janela de herois.')
         click(x,y)
         time.sleep(random.uniform(2,3))
 
     if pyautogui.locateOnScreen('./src/inGame.png') != None and workOn == False:
         backBtn = pyautogui.locateOnScreen('./src/backButton.png', grayscale=True, confidence=0.8)
         x,y = pyautogui.center(backBtn)
-        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Você me iniciou no modo Teasure Hunt. \n Vou voltar para o menu principal é começar novamente.')
+        botSay('Você me iniciou no modo Teasure Hunt. \n Vou voltar para o menu principal é começar novamente.')
         click(x,y)
         time.sleep(random.uniform(2,3))
 
@@ -142,7 +153,7 @@ while checkServerStatus():
     if pyautogui.locateOnScreen('./src/teasureHunt.png', grayscale=True, confidence=0.8) != None and workOn == True:
         teasureHunt = pyautogui.locateOnScreen('./src/teasureHunt.png', grayscale=True, confidence=0.8)
         x,y = pyautogui.center(teasureHunt)
-        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Indo para a TeasureHunt.')
+        botSay('Indo para a TeasureHunt.')
         click(x,y)
         time.sleep(random.uniform(5,10))
 
@@ -151,9 +162,9 @@ while checkServerStatus():
         time_start = time.time()
 
         while time.time() - time_start < time_duration:
-            print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Aguardando Herois Trabalhar.')
+            botSay('Aguardando Herois Trabalhar.')
             if pyautogui.locateOnScreen('./src/newMap.png') != None:
-                print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Mapa Completo! Aguardando nova TeasureHunt. Total de Mapas Completos: ' + str(mapCount))
+                botSay('Mapa Completo! Aguardando nova TeasureHunt. Total de Mapas Completos: ' + str(mapCount))
                 mapCount += 1
                 time.sleep(random.uniform(12,15))
             time.sleep(60)
@@ -161,10 +172,11 @@ while checkServerStatus():
         #Volta para o menu principal para "salvar" o progresso.
         backBtn = pyautogui.locateOnScreen('./src/backButton.png', grayscale=True, confidence=0.8)
         x,y = pyautogui.center(backBtn)
-        print(time.strftime(" %H:%M:%S") + ' - [Bombot] > Voltando para o menu principal.')
+        botSay('Voltando para o menu principal.')
         click(x,y)
         time.sleep(random.uniform(2,3))
 
-    print("========================== "+time.strftime(" %H:%M:%S")+" ==========================")
+    print("========================== "+time.strftime(timeString)+" ==========================")
     print('             Total de Mapas Concluidos: ' + str(mapCount))
     print('===============================================================')
+    
